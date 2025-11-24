97e9c7a61082f42a058fc19a6151686c2bb5c59a
ssannejohansson <sannej0hansson@hotmail.com>
2025-11-24 10:29:54 +0100

    Set up and test webhoks

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -27,7 +27,10 @@ SECRET_KEY = 'django-insecure-(a@(_b@_9n!=mczx-w37r0di=i$kz91i$bl073*5z02@9azyj-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+]
 
 
 # Application definition
@@ -176,6 +179,7 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
